985ce96a22a061caea65ba39c560be7293b051ea
sandeep prajapati <167186210+sandeep8128@users.noreply.github.com>
2025-09-04 13:11:51 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 233074
+let accountEmail = "sandeep@google.com"
+var accountPassword = "12345"
+
+accountCity = "jaipur"
+
+//accountId = 343253  //not allowe because const value do not change 
+
+console.log(accountId);
+
+accountEmail = "hdfc@gmail.com"
+accountPassword = "638473"
+accountCity = "gurugram"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
