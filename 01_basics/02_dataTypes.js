49bc5bd5dc128fd3878bea24b4803eaf11e1ac93
sandeep prajapati <167186210+sandeep8128@users.noreply.github.com>
2025-09-05 06:35:21 +0000

    dataTypes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+// number => 2 to power 53 it is rangae of number
+//bigint 
+// string => " "
+// boolean => true / flase
+// null = it is type of object
+// undefined = it is type of undefined
+
+// symbol => unique
+
+console.log(typeof null);  // output = object
+console.log(typeof undefined); // output = undefined
